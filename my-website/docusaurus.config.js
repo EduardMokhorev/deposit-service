b53cc21bcf3819ccac57fd853dbac70cf9582038
Eduard Mokhorev <mokhorev.eduard.cd@gmail.com>
2024-12-06 10:46:15 +0300

    fist check

---
+++ my-website/docusaurus.config.js
@@ -0,0 +1,151 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+const simplePlantUML = require("@akebifiky/remark-simple-plantuml"); // объявляем плагин для plantuml
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** 
+ * Открой файл docusaurus.config.js и внеси следующие изменения:
+url: 'https://<username>.github.io'  - имя пользователя на github.
+baseUrl: <repository-name> - имя репозитория.
+organizationName: <username> - ваше имя пользователя
+projectName: <repository-name> - имя репозитория
+onBrokenLinks: 'warn'
+onBrokenMarkdownLinks: 'warn'
+trailingSlash: false
+deploymentBranch: 'gh-pages' - ветка, на которой будет развернут сайт, это ветка по умолчанию для GitHub Pages.
+blog: false - можешь отключить блог
+ * 
+ */
+
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+  url: 'https://EduardMokhorev.github.io',
+  baseUrl: 'deposit-service',
+  organizationName: 'EduardMokhorev', // Usually your GitHub org/user name.
+  projectName: 'deposit-service', // Usually your repo name.
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+  
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  //подключаем плагин для drawio
+  plugins: [
+    ['drawio', {}]
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: 'docs',
+          editUrl:
+            'https://github.com/itikineva/doc-as-a-code-workshop',
+            remarkPlugins: [simplePlantUML], //подключаем плагин для plantuml
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
